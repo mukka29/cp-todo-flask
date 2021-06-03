3042c700db508e0280ea28292eb9a1b1b4018b84
mukka29 <mukkadev@gmail.com>
2021-06-03 10:40:22 -0500

    Working CRUD functionality

--- app.py
+++ app.py
@@ -11,18 +11,45 @@ class Todo(db.Model):
     text = db.Column(db.String(200))
     complete = db.Column(db.Boolean)
 
+
 @app.route('/')
 def index():
     incomplete = Todo.query.filter_by(complete=False).all()
-    return render_template('index.html', incomplete=incomplete)
+    complete = Todo.query.filter_by(complete=True).all()
+    return render_template('index.html', incomplete=incomplete, complete=complete)
+
 
 @app.route('/add', methods=['POST'])
 def add():
     todo = Todo(text=request.form['todoitem'], complete=False)
     db.session.add(todo)
     db.session.commit()
-
     return redirect(url_for('index'))
+
+
+@app.route('/complete/<id>')
+def complete(id):
+    todo = Todo.query.filter_by(id=int(id)).first()
+    todo.complete = True
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/uncheck/<id>')
+def uncheck(id):
+    todo = Todo.query.filter_by(id=int(id)).first()
+    todo.complete = False
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<id>')
+def delete(id):
+    todo = Todo.query.filter_by(id=int(id)).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
